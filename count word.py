0ae6b40858f06643cf6deea22048372e2417a991
jc490479 <kunbo.zhu@my.jcu.edu.au>
2018-12-04 15:15:14 +0800

    prac work

---
+++ count word.py
@@ -0,0 +1,13 @@
+print("Please enter a string")
+
+import collections
+str1 = ['a','b','c','d','a','b','c']
+m = collections.Counter(str1)
+print(str1)
+print(m)
+print(m['a'])
+print(m['b'])
+
+
+
+print("hello world")
